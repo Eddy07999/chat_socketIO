2e0741c0fed5188db1fe970d67593ab981af69e5
leullier <eddy.leullier@gmail.com>
2022-12-23 14:46:11 +0100

    server_is_running

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const {Server} = require('socket.io');
+const cors = require('cors');
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        method:['GET','POST'],
+    },
+});
+
+server.listen(3001, () => {
+    console.log('listening on port 3001')
+});
+
+
